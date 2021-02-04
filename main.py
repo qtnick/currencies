ff030e148a5cd249f81391577050567ab64f22c4
Jakub Kutnik <jakubkutnik@gmail.com>
2021-02-04 17:37:44 +0100

    Add class

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ from datetime import datetime
 from pytz import timezone
 
 
-now_poland = datetime.now(timezone('Europe/Warsaw'))
+class ExchangeRateScraper:
+    def __init__(self):
+        self.session = HTMLSession()
+        self.interval_in_sec = 10
 
-print('Ctrl+c to exit.\n')
+    def run_forever(self):
+        print('Ctrl+c to exit.\n')
+        while True:
+            try:
+                self.print_usd_exchange_rate()
+                time.sleep(self.interval_in_sec)
+            except KeyboardInterrupt:
+                print('\nInterrupted.')
+                exit(0)
+
+    def print_usd_exchange_rate(self):
+        response = self.session.get('https://internetowykantor.pl/kurs-dolara/')
+        ex_rate = response.html.find('span.kurs_sredni', first=True)
+        print(f'Current USD-PLN exchange rate: {ex_rate.text} ({self.current_time()})')
+
+    @staticmethod
+    def current_time():
+        current_time = datetime.now(timezone('Europe/Warsaw'))
+        return current_time.strftime("%H:%M:%S %d-%m-%y")
 
 
-def usd_to_pln():
-    session = HTMLSession()
-    response = session.get('https://internetowykantor.pl/kurs-dolara/')
-
-    ex_rate = response.html.find('span.kurs_sredni', first=True)
-    print(f'Current USD-PLN exchange rate: {ex_rate.text} ({now_poland.strftime("%H:%M:%S %d-%m-%y")})')
-
-
-while True:
-    try:
-        usd_to_pln()
-        time.sleep(10)
-    except KeyboardInterrupt:
-        print('\nInterrupted.')
-        break
+if __name__ == "__main__":
+    ExchangeRateScraper().run_forever()
