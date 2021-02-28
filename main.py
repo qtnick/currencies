1fc369cd809b8af37e40c1247092bf29b9ffb8a2
Jakub Kutnik <jakubkutnik@gmail.com>
2021-02-28 10:25:15 +0100

    Add database

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from requests_html import HTMLSession
 import time
 from datetime import datetime
 from pytz import timezone
+import sqlite3
+
+
+conn = sqlite3.connect('exchange_rates.db')
+
+c = conn.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS exchange_rates (
+            rate real,
+            date real
+            )""")
+
+
+def insert_rate(rate, date):
+    with conn:
+        c.execute("INSERT INTO exchange_rates VALUES (:rate, :date)", {'rate': rate, 'date': date})
 
 
 class ExchangeRateScraper:
@@ -10,7 +26,7 @@ class ExchangeRateScraper:
         self.interval_in_sec = 10
 
     def run_forever(self):
-        print('Ctrl+c to exit.\n')
+        print('\nCtrl+c to exit.\n')
         while True:
             try:
                 self.print_usd_exchange_rate()
@@ -23,6 +39,7 @@ class ExchangeRateScraper:
         response = self.session.get('https://internetowykantor.pl/kurs-dolara/')
         ex_rate = response.html.find('span.kurs_sredni', first=True)
         print(f'Current USD-PLN exchange rate: {ex_rate.text} ({self.current_time()})')
+        insert_rate(ex_rate.text, self.current_time())
 
     @staticmethod
     def current_time():
@@ -32,3 +49,6 @@ class ExchangeRateScraper:
 
 if __name__ == "__main__":
     ExchangeRateScraper().run_forever()
+
+conn.commit()
+conn.close()
